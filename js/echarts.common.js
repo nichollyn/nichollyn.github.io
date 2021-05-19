d0eaac4b60adf39a5e2b5fe6d3b5e8bed9e9db72
kevin.lin <nichollyn@gmail.com>
2021-05-19 14:35:18 +0800

    Site updated: 2021-05-19 14:35:17

--- js/echarts.common.js
+++ js/echarts.common.js
@@ -41,22 +41,4 @@ import './src/component/dataZoom';
 import './src/component/toolbox';
 
 import 'zrender/src/vml/vml';
-import 'zrender/src/svg/svg';<script>
-        document.querySelectorAll('.github-emoji')
-          .forEach(el => {
-            if (!el.dataset.src) { return; }
-            const img = document.createElement('img');
-            img.style = 'display:none !important;';
-            img.src = el.dataset.src;
-            img.addEventListener('error', () => {
-              img.remove();
-              el.style.color = 'inherit';
-              el.style.backgroundImage = 'none';
-              el.style.background = 'none';
-            });
-            img.addEventListener('load', () => {
-              img.remove();
-            });
-            document.body.appendChild(img);
-          });
-      </script>
+import 'zrender/src/svg/svg';
